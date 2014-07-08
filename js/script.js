d47c18e09750b31eeb337d01ab8b575fb240394e
Till Schander <hello@tillschander.com>
2014-07-09 01:02:27 +0200

    first version with a sound

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
+var sounds = [];
+
+function init() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+
+    bufferLoader = new BufferLoader(
+        context,
+        [
+          'media/sample1.mp3'
+        ],
+        finishedLoading
+      );
+
+    bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+    sounds = bufferList;
+}
+
+function playSound(number) {
+    var source = context.createBufferSource();
+    source.buffer = sounds[number];
+    source.connect(context.destination);
+    source.start(0);
+}
+
 $('.pad').mousedown(function() {
     $(this).addClass('pressed');
+    playSound(0);
 });
 
 $('html').mouseup(function() {
     $('.pad').removeClass('pressed');
 });
+
+init();
